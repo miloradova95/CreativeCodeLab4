45e937547cc4ab618d4f352b24d5a8ede9258aef
Aleksandar Miloradovic <amiloradovic95@gmail.com>
2025-06-20 20:58:47 +0200

    finalCommit

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,6 +17,7 @@ namespace AK
         static const AkUniqueID BEACHWAVES = 2425448196U;
         static const AkUniqueID CANTUNLOCKDOOR = 1002233763U;
         static const AkUniqueID CHURCHBELLS = 3818553682U;
+        static const AkUniqueID CRY = 731352139U;
         static const AkUniqueID DAMAGESOUND = 3254952287U;
         static const AkUniqueID DEATH = 779278001U;
         static const AkUniqueID DIG = 445985471U;
